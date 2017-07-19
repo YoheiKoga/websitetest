e6e07d8636cadbe2f43194dced0326f40db832fc
yoheikoga <prawv2@gmail.com>
2017-07-19 16:54:37 +0900

    add three.js TextGeometry Sample

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # website test
 
 This website is created on Github Pages
+
+https://yoheikoga.github.io/websitetest
---
+++ docs/threejs_text_test/main.js
@@ -0,0 +1,46 @@
+var scene = new THREE.Scene();
+var aspect = window.innerWidth / window.innerHeight;
+var camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+var controls = new THREE.OrbitControls(camera);
+
+var axis = new THREE.AxisHelper(1000);
+var light = new THREE.DirectionalLight(0xb4e7f2, 1.5);
+
+light.position.set(1,1,1);
+light.target.position.set(0,0,0);
+
+scene.add(axis);
+scene.add(light);
+scene.add(light.target);
+
+renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+document.body.appendChild(renderer.domElement);
+
+camera.position.set(100, 50, 200);
+
+
+var loader = new THREE.FontLoader();
+loader.load('helvetiker_regular.typeface.json', function(font){
+	var textGeometry = new THREE.TextGeometry("Hello Three.js!", {
+		font: font,
+		size: 20,
+		height: 5,
+		curveSegments: 12
+	});
+	var materials = [
+		new THREE.MeshBasicMaterial( { color: Math.random() * 0xffffff, overdraw: 0.5 } ),
+		new THREE.MeshBasicMaterial( { color: 0x000000, overdraw: 0.5 } )
+	];
+	var textMesh = new THREE.Mesh(textGeometry, materials);
+	scene.add(textMesh);
+});
+
+var render = function () {
+	requestAnimationFrame(render);
+	controls.update();
+	renderer.setClearColor(0xaabbcc, 1.0);
+	renderer.render(scene, camera);
+};
+
+render();
